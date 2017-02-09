118ada197cdc4e20d65ad020995b9629fe5e2311
Derek Manwaring <derekmn@uw.edu>
2017-02-09 11:43:29 -0800

    Add outbound dropper as well

--- firewall.c
+++ firewall.c
@@ -1,9 +1,7 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 
-static struct nf_hook_ops nfho;
 
 unsigned int hook_func(void *priv,
         struct sk_buff *skb,
@@ -11,18 +9,29 @@ unsigned int hook_func(void *priv,
     return NF_DROP;
 }
 
+static struct nf_hook_ops inbound_hook;
+static struct nf_hook_ops outbound_hook;
+
 int init_module(void) {
-    printk(KERN_INFO "Hello World!\n");
-    nfho.hook = hook_func;
-    nfho.hooknum = NF_INET_PRE_ROUTING;
-    nfho.pf = PF_INET;
-    nfho.priority = NF_IP_PRI_FIRST;
-    nf_register_hook(&nfho);
+    printk(KERN_INFO "Starting firewall.\n");
+
+    inbound_hook.hook = drop_unless_whitelisted;
+    inbound_hook.hooknum = NF_INET_PRE_ROUTING;
+    inbound_hook.pf = PF_INET;
+    inbound_hook.priority = NF_IP_PRI_FIRST;
+    nf_register_hook(&inbound_hook);
+
+    outbound_hook.hook = drop_unless_whitelisted;
+    outbound_hook.hooknum = NF_INET_LOCAL_OUT;
+    outbound_hook.pf = PF_INET;
+    outbound_hook.priority = NF_IP_PRI_FIRST;
+    nf_register_hook(&outbound_hook);
+
     return 0;
 }
 
 void cleanup_module(void) {
-    printk(KERN_INFO "Bye-bye World!.\n");
-    nf_unregister_hook(&nfho);
+    printk(KERN_INFO "Turning off firewall.\n");
+    nf_unregister_hook(&inbound_hook);
+    nf_unregister_hook(&outbound_hook);
 }
-
